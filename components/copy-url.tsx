234e3d262407953243f9097c132ce59a70d1a329
Hasun Nilupul <hasunnilupul16@gmail.com>
2025-02-13 23:58:21 +0530

    refactor: copy shortened url page

--- components/copy-url.tsx
+++ components/copy-url.tsx
@@ -14,44 +14,46 @@ export default function CopyUrl() {
       </div>
 
       <div className="mt-5 w-full py-2 px-3 sm:py-4 sm:px-8 bg-white rounded-md shadow">
-        <label htmlFor="link" className="sr-only">
-          Link
-        </label>
-        <div className="flex items-center rounded-md bg-white h-10 sm:h-14 outline outline-1 -outline-offset-1 outline-gray-300">
-          <input
-            id="link"
-            name="link"
-            type="url"
-            className="flex-grow block h-full min-w-0 rounded-l-md py-1.5 pl-1.5 sm:pl-3 text-sm/6 sm:text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-gray-800"
-          />
-          <div className="shrink-0 flex h-full focus-within:relative">
-            <button
-              type="button"
-              className="block px-1.5 sm:px-3 bg-sky-600 text-white text-base sm:text-lg font-medium rounded-r-md"
-            >
-              Copy URL
-            </button>
+        <div className="flex flex-col">
+          <label htmlFor="link" className="sr-only">
+            Link
+          </label>
+          <div className="flex items-center rounded-md bg-white h-10 sm:h-14 outline outline-1 -outline-offset-1 outline-gray-300">
+            <input
+              id="link"
+              name="link"
+              type="url"
+              className="flex-grow block h-full min-w-0 rounded-l-md py-1.5 pl-1.5 sm:pl-3 text-sm/6 sm:text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-gray-800"
+            />
+            <div className="shrink-0 flex h-full focus-within:relative">
+              <button
+                type="button"
+                className="block px-1.5 sm:px-3 bg-sky-600 text-white text-base sm:text-lg font-medium rounded-r-md"
+              >
+                Copy URL
+              </button>
+            </div>
           </div>
+
+          <p className="mt-3 w-fit text-xs sm:text-sm font-medium text-gray-900 sm:max-w-lg">
+            <span className="mr-1">Long URL:</span>
+            <a
+              href="long-url"
+              target="_blank"
+              className="text-sky-500 hover:underline text-wrap"
+            >
+              long-url
+            </a>
+          </p>
+
+          <Link
+            href="/"
+            className="block mt-4 w-fit rounded-md bg-sky-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600"
+          >
+            Shorten another URL
+          </Link>
         </div>
       </div>
-
-      <p className="mt-3 text-xs sm:text-sm font-medium text-gray-900 sm:max-w-lg">
-        <span className="mr-1">Long URL:</span>
-        <a
-          href="long-url"
-          target="_blank"
-          className="text-sky-500 hover:underline"
-        >
-          long-url
-        </a>
-      </p>
-
-      <Link
-        href="/"
-        className="mt-4 rounded-md bg-sky-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600"
-      >
-        Shorten another URL
-      </Link>
     </section>
   );
 }
